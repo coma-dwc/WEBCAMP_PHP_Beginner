ec6f4d5350d1cdad9e715885dcc856e9af46fc9b
coma <tamagomapon@gmail.com>
2023-09-09 11:02:34 +0000

    define.php

---
+++ define.php
@@ -0,0 +1,12 @@
+<?php
+#定数：あらかじめ決まっていて、途中で中身を変更できない値
+
+//定数の定義
+define('DEF_INT', 123); //整数
+define('DEF_FLOAT', 3.14); //小数
+define('DEF_STRING', "string"); //文字列
+
+//定数の使用
+var_dump(DEF_INT);
+var_dump(DEF_FLOAT);
+var_dump(DEF_STRING);
