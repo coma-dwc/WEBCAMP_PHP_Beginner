d9640849592d4647986446097d1285dec85a1710
coma <tamagomapon@gmail.com>
2023-09-09 09:38:30 +0000

    string_add.php

---
+++ string_add.php
@@ -0,0 +1,16 @@
+<?php
+#文字列結合
+//元ネタ
+$string_1 = "Hello ";
+$string_2 = "world.";
+
+//.(ドット)を使った結合
+$string_dot = $string_1 . $string_2 . "\n";
+echo $string_dot;
+
+//ダブルクォートを使った変数展開
+$string_dquote = "{$string_1}{$string_2} \n";
+echo $string_dquote;
+
+
+//※PHPでは + で文字列の結合はできない
